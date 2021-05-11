0f070d68f9fb9316bde26cb619ac08710c690d77
Quentin <quentin1998-007@hotmail.fr>
2021-05-11 17:38:47 +0200

    Création du visuel de la connectionActivity

--- app/src/main/java/com/eseo/getmyspot/view/main/fragments/AddSpotFragment.kt
+++ app/src/main/java/com/eseo/getmyspot/view/main/fragments/AddSpotFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.eseo.getmyspot.R
 import com.eseo.getmyspot.data.preferences.LocalPreferences
+import com.eseo.getmyspot.view.connection.ConnectionActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.NonCancellable.cancel
 
 class AddSpotFragment : Fragment() {
 
@@ -25,18 +28,18 @@ class AddSpotFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        if (context?.let {
-                LocalPreferences.getInstance(it).getStringValue(LocalPreferences.PSEUDO)
-            } == null) {
-            context?.let {
-                MaterialAlertDialogBuilder(it).setTitle(getString(R.string.require_connection_title))
-                    .setCancelable(false)
-                    .setMessage(R.string.require_connection_message)
-                    .setPositiveButton(getString(R.string.log_in)) { dialog, which ->
-                        // TODO : faire la redirection vers l'activity de connection
-                    }
-                    .show()
-            }
+        if (LocalPreferences.getInstance(requireContext())
+                .getStringValue(LocalPreferences.PSEUDO) == null
+        ) {
+            MaterialAlertDialogBuilder(requireContext()).setTitle(getString(R.string.require_connection_title))
+                .setCancelable(false)
+                .setMessage(R.string.require_connection_message)
+                .setPositiveButton(getString(R.string.log_in)) { dialog, which ->
+                    startActivity(ConnectionActivity.getStartIntent(requireContext()))
+                }.setNegativeButton(getText(R.string.cancel)) { dialog, which ->
+                    findNavController().navigate(R.id.action_add_spot_to_home)
+                }
+                .show()
         }
     }
 
