a2a16cf6025032bcb2e52e8935ff8f3605d1c88f
Quentin <quentin1998-007@hotmail.fr>
2021-05-19 21:54:04 +0200

    Une fois que le compte est créer : on quitte la page + on toast success

--- app/src/main/java/com/eseo/getmyspot/view/account/signup/SignupActivity.kt
+++ app/src/main/java/com/eseo/getmyspot/view/account/signup/SignupActivity.kt
@@ -3,18 +3,19 @@ package com.eseo.getmyspot.view.account.signup
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import com.eseo.getmyspot.R
-import com.eseo.getmyspot.databinding.ActivitySigninBinding
 import com.eseo.getmyspot.databinding.ActivitySignupBinding
 import com.eseo.getmyspot.view.Failed
-import com.eseo.getmyspot.view.Loading
-import com.eseo.getmyspot.view.account.signin.SigninActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -39,12 +40,18 @@ class SignupActivity : AppCompatActivity() {
     private fun setupUi() {
         createAccountViewModel.states.observe(this, Observer { state ->
             when (state) {
-                is Loading -> Toast.makeText(this, "loading..", Toast.LENGTH_SHORT).show()
-                is CreateAccountViewModel.CallResult -> Toast.makeText(
-                    this,
-                    "FINISH : " + state.isAccountCreate,
-                    Toast.LENGTH_SHORT
-                ).show()
+                //is Loading -> TODO : peut être mettre un logo de chargement plus tard
+                is CreateAccountViewModel.CallResult ->
+                    if (state.isAccountCreate) {
+                        Toast.makeText(this, getString(R.string.success), Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        MaterialAlertDialogBuilder(this).setTitle(getString(R.string.create_account_error_title))
+                            .setMessage(getString(R.string.create_account_error_message))
+                            .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                            }
+                            .show()
+                    }
                 is Failed -> Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show()
             }
         })
@@ -59,8 +66,11 @@ class SignupActivity : AppCompatActivity() {
             }
         }
         binding.btnCreateAccount.setOnClickListener {
-            if(passwordValidation)
-                createAccountViewModel.doRemoteTestAction(binding.txtInputPseudo.text.toString(), binding.txtInputPassword.text.toString())
+            if (passwordValidation)
+                createAccountViewModel.doRemoteTestAction(
+                    binding.txtInputPseudo.text.toString(),
+                    binding.txtInputPassword.text.toString()
+                )
             else
                 Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show()
         }
