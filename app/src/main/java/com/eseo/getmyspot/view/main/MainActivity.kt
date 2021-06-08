c5aee9f66ff5bc0636bac9056bf880ced05a1047
Quentin <quentin1998-007@hotmail.fr>
2021-06-08 21:31:45 +0200

    On vérifie si l'utilisateur à accès à internet. Si ce n'est pas le cas, on ferme l'application

--- app/src/main/java/com/eseo/getmyspot/view/main/MainActivity.kt
+++ app/src/main/java/com/eseo/getmyspot/view/main/MainActivity.kt
@@ -2,12 +2,15 @@ package com.eseo.getmyspot.view.main
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.eseo.getmyspot.R
 import com.eseo.getmyspot.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.net.InetAddress
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         // récuperer le contexte pour changer d'activity
-        fun getStartIntent(context: Context) : Intent {
+        fun getStartIntent(context: Context): Intent {
             return Intent(context, MainActivity::class.java)
         }
     }
@@ -29,9 +32,44 @@ class MainActivity : AppCompatActivity() {
         setUpNavigation()
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        //On check si l'utilisateur à internet, dans le cas contraire on interdit l'utilisation de l'application
+        val thread = Thread {
+            if (!isInternetAvailable()) {
+                runOnUiThread {
+                    MaterialAlertDialogBuilder(this).setTitle(getString(R.string.require_internet_title))
+                        .setCancelable(false)
+                        .setMessage(R.string.require_internet_message)
+                        .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                            finish()
+                        }
+                        .show()
+                }
+            }
+        }
+
+        thread.start()
+    }
+
     fun setUpNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navHostFragment!!.navController)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            navHostFragment!!.navController
+        )
+    }
+
+    fun isInternetAvailable(): Boolean {
+        return try {
+            val ipAddr: InetAddress = InetAddress.getByName("google.com")
+            //You can replace it with your name
+            !ipAddr.equals("")
+        } catch (e: Exception) {
+            false
+        }
     }
 }
 
